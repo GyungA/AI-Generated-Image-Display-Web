a4b44c0fc8666a526aae174aaa040d2474a84e9e
GyungA <gga612@naver.com>
2024-02-23 16:58:10 +0900

    feat: BaseEntity에 JpaAuditing 추가

--- src/main/java/com/gyunga/project/aigeneratedimagedisplayweb/board/controller/BoardController.java
+++ src/main/java/com/gyunga/project/aigeneratedimagedisplayweb/board/controller/BoardController.java
@@ -3,12 +3,14 @@ package com.gyunga.project.aigeneratedimagedisplayweb.board.controller;
 import com.gyunga.project.aigeneratedimagedisplayweb.board.entity.Board;
 import com.gyunga.project.aigeneratedimagedisplayweb.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@EnableJpaAuditing
 public class BoardController {
     @Autowired
     BoardService boardService;
