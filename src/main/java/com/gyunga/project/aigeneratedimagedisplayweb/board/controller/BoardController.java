28a8449313b3f704194a72c62e4139eac1b884ce
GyungA <gga612@naver.com>
2024-02-23 16:50:02 +0900

    feat: BoardController에 @PostMapping 추가

--- src/main/java/com/gyunga/project/aigeneratedimagedisplayweb/board/controller/BoardController.java
+++ src/main/java/com/gyunga/project/aigeneratedimagedisplayweb/board/controller/BoardController.java
@@ -1,16 +1,27 @@
 package com.gyunga.project.aigeneratedimagedisplayweb.board.controller;
 
 import com.gyunga.project.aigeneratedimagedisplayweb.board.entity.Board;
+import com.gyunga.project.aigeneratedimagedisplayweb.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BoardController {
+    @Autowired
+    BoardService boardService;
 
     @GetMapping("createBoard")
     public String createBoard(Model model) {
         model.addAttribute("page_name", "createBoard");
+        return "board-create-and-update";
+    }
+
+    @PostMapping("createBoard")
+    public String createBoard(Board board) {
+        boardService.create(board);
         return "board-create-and-update";
     }
     public void readBoard() {
