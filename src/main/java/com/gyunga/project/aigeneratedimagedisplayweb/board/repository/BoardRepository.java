c43579e0e508a11d5a81d6bd43478fe69668cd17
GyungA <gga612@naver.com>
2024-02-23 13:09:57 +0900

    feat: add interface and implement of Board Repository

---
+++ src/main/java/com/gyunga/project/aigeneratedimagedisplayweb/board/repository/BoardRepository.java
@@ -0,0 +1,7 @@
+package com.gyunga.project.aigeneratedimagedisplayweb.board.repository;
+
+public interface BoardRepository {
+    void save();
+    void findById();
+    void findAll();
+}
---
+++ src/main/java/com/gyunga/project/aigeneratedimagedisplayweb/board/repository/JpaBoardRepository.java
@@ -0,0 +1,12 @@
+package com.gyunga.project.aigeneratedimagedisplayweb.board.repository;
+
+import com.gyunga.project.aigeneratedimagedisplayweb.board.entity.Board;
+
+public class JpaBoardRepository implements BoardRepository {
+    @Override
+    public void save() {};
+    @Override
+    public void findById() {};
+    @Override
+    public void findAll() {};
+}
