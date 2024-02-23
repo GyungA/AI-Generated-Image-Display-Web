f8c0734e5a0427a1ef33ee2f694ea2c799dad759
GyungA <gga612@naver.com>
2024-02-23 16:41:29 +0900

    feat: JpaBoardRepository, BoardService 생성 및 빈 등록

--- src/main/java/com/gyunga/project/aigeneratedimagedisplayweb/board/repository/JpaBoardRepository.java
+++ src/main/java/com/gyunga/project/aigeneratedimagedisplayweb/board/repository/JpaBoardRepository.java
@@ -1,10 +1,12 @@
 package com.gyunga.project.aigeneratedimagedisplayweb.board.repository;
 
 import com.gyunga.project.aigeneratedimagedisplayweb.board.entity.Board;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class JpaBoardRepository implements BoardRepository {
     @Override
-    public void save() {};
+    public void save(Board board) {};
     @Override
     public void findById() {};
     @Override
--- src/main/java/com/gyunga/project/aigeneratedimagedisplayweb/board/service/BoardService.java
+++ src/main/java/com/gyunga/project/aigeneratedimagedisplayweb/board/service/BoardService.java
@@ -1,10 +1,17 @@
 package com.gyunga.project.aigeneratedimagedisplayweb.board.service;
 
 import com.gyunga.project.aigeneratedimagedisplayweb.board.entity.Board;
+import com.gyunga.project.aigeneratedimagedisplayweb.board.repository.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BoardService {
+    private final BoardRepository boardRepository;
+    @Autowired
+    public BoardService(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
 
     public Long create(Board board) {
         return board.getId();
